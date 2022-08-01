ecd44658ea5e798bcd8facd5fea46954421eefcd
mtsadler-branch <101577043+mtsadler-branch@users.noreply.github.com>
2022-08-01 17:47:44 -0500

    bump version to 0.1.0

---
+++ py-github-helper/__main__.py
@@ -0,0 +1,12 @@
+import logging
+import sys
+from .custom_arg_parser import parse_args
+
+
+def main(argv):
+    parse_args(argv)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.DEBUG)
+    print(main(sys.argv[1:]))
